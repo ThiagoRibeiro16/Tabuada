188f67605da68ea309f8c9f4ad672aec5d91dab1
Thiago Ribeiro <thiago.escola321@gmail.com>
2025-06-01 23:53:34 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int contador = 0;
+int numero;
+int resultado;
+
+Console.Write("Digite um número: ");
+numero = int.Parse(Console.ReadLine()!);
+
+while (contador <= 10)
+{
+    resultado = numero * contador;
+    Console.WriteLine($"{numero} x {contador} = {resultado}");
+    contador++;
+}
